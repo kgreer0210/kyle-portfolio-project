eb3365b2f5d5ea2cba7d0f4e95de1fd248c165e3
Kyle Greer <kylegreer.kg@gmail.com>
2026-03-14 10:44:52 -0400

    feat: add dynamic export to blog page for improved rendering

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from "next";
 import BlogClient from "./BlogClient";
 import { getBlogPosts } from "@/lib/blog";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: "Blog | KYGR Solutions",
   description:
