63bef5253df77b17176a594f429c5efc0ad34008
Kyle Greer <kylegreer.kg@gmail.com>
2026-07-08 08:25:34 -0400

    Update webauthn config

--- src/lib/webauthn/config.ts
+++ src/lib/webauthn/config.ts
@@ -1,5 +1,5 @@
 export const webAuthnConfig = {
-  rpID: process.env.WEBAUTHN_RP_ID ?? "localhost",
+  rpID: process.env.WEBAUTHN_RP_ID ?? "kygrsolutions.com",
   rpName: process.env.WEBAUTHN_RP_NAME ?? "Kyle Greer Portal",
-  origin: process.env.WEBAUTHN_ORIGIN ?? "http://localhost:3000",
+  origin: process.env.WEBAUTHN_ORIGIN ?? "https://kygrsolutions.com",
 } as const;
